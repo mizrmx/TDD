0eae2b021be0cf11d21ab2cb0953af2ea33812a9
Mayra Zurita <mizrmx@gmail.com>
2021-10-13 11:11:43 -0500

    Agregando introduccion jest

---
+++ jest-introduccion/src/calculator/calculator.test.js
@@ -0,0 +1,14 @@
+import { calculator } from ".";
+
+test("sum calculator", () => {
+    const result = calculator.sum(1, 2);
+    expect(result).toBe(3);
+});
+
+test.todo("substrack calculator");
+
+test.todo("multiply calculator");
+
+test.todo("divide calculator");
+
+//npm install --save-dev babel-jest @babel/core @babel/preset-env
